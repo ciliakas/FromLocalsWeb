f6a9454b55130f8ad5e22e07b9bcc95636da748c
Smaznovas Karolis <karolis.smaznovas@mif.stud.vu.lt>
2020-12-15 14:44:35 +0200

    Localized enum. Also added white spaces for words: "CarRepair" -> "Car repair", "RuralTourism" -> "Rural tourism". In map vendor types now available in Lithuanian language.

---
+++ FromLocalsToLocals/Utilities/CultureHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Web.SessionState;
+using System.Threading;
+using System.Globalization;
+
+namespace FromLocalsToLocals.Utilities
+{
+    public class CultureHelper
+    {
+
+        public static int CurrentCulture
+        {
+            get
+            {
+                if (Thread.CurrentThread.CurrentUICulture.Name == "en")
+                {
+                    return 0;
+                }
+                else if (Thread.CurrentThread.CurrentUICulture.Name == "lt")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                if (value == 0)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+                }
+                else if (value == 1)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("lt");
+                }
+                else
+                {
+                    Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+                }
+                Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+            }
+        }
+    }
+}
--- FromLocalsToLocals/Utilities/Enums/Types.cs
+++ FromLocalsToLocals/Utilities/Enums/Types.cs
@@ -1,19 +1,37 @@
-﻿
+﻿using FromLocalsToLocals;
+using Microsoft.Extensions.Localization;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace FromLocalsToLocals.Utilities
 {
+
+    [TypeConverter(typeof(Resources.VendorTypeEnum))]
     public enum VendorType
     {
-        Food,           
+        [Display(Name = "Food", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
+        Food,
+        [Display(Name = "CarRepair",Description = "Car Repair",ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         CarRepair,
+        [Display(Name = "Beverages", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Beverages,
+        [Display(Name = "Clothing", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Clothing,
+        [Display(Name = "Electronics", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Electronics,
+        [Display(Name = "RuralTourism", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         RuralTourism,
+        [Display(Name = "Footwear", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Footwear,
+        [Display(Name = "Stationery", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Stationery,
+        [Display(Name = "Toys", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Toys,
+        [Display(Name = "Music", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Music,
+        [Display(Name = "Flowers", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Flowers,
+        [Display(Name = "Other", ResourceType = typeof(FromLocalsToLocals.Resources.VendorTypeEnum))]
         Other
     }
     public enum OrderType
@@ -21,4 +39,6 @@ namespace FromLocalsToLocals.Utilities
         MostLiked,
         LeastLiked
     }
+
+
 }
