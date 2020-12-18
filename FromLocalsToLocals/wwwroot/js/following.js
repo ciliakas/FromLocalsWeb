7073f33696b7c8307cfab037992b35fa09b2633a
Rangatin <justinas.maniusis@mif.stud.vu.lt>
2020-12-18 22:37:17 +0200

    Fixes for following

--- FromLocalsToLocals/wwwroot/js/following.js
+++ FromLocalsToLocals/wwwroot/js/following.js
@@ -1,5 +1,7 @@
 ﻿function followBtnClick(obj, id) {
-    var fButton = $(`.followBtn[data-id='${id}']`);
+    var fButton = document.getElementById("followButn");
+    var followerCount = document.getElementById("followerCount");
+
     $.ajax({
         type: "POST",
         url: '/Follower/' + obj.innerText + '/' + id,
@@ -10,18 +12,23 @@
                 if (obj.innerText == "Follow") {
                     fButton.innerHTML = "Unfollow";
                     fButton.className = "btn btn-outline-primary w-100 followBtn";
+                    followerCount.innerHTML = parseInt(followerCount.innerHTML, 10) + 1;
                 }
+
                 else if (obj.innerText == "Sekti") {
                     fButton.innerHTML = "Nebesekti";
                     fButton.className = "btn btn-outline-primary w-100 followBtn";
+                    followerCount.innerHTML = parseInt(followerCount.innerHTML, 10) + 1;
                 }
                 else if (obj.innerText == "Unfollow") {
                     fButton.innerHTML = "Follow";
                     fButton.className = "btn btn-primary w-100 followBtn";
+                    followerCount.innerHTML = parseInt(followerCount.innerHTML, 10) - 1;
                 }
                 else if (obj.innerText == "Nebesekti") {
                     fButton.innerHTML = "Sekti";
                     fButton.className = "btn btn-primary w-100 followBtn";
+                    followerCount.innerHTML = parseInt(followerCount.innerHTML, 10) - 1;
                 }
             } else {
                 alert("Error occured when trying to " + obj.innerText + " vendor");
