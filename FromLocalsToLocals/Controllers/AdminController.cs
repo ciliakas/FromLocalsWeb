416effbba33624a58e087b4b07831d06b77d154b
unknown <gytis.becalis@mif.stud.vu.lt>
2021-04-02 22:39:13 +0300

    Admin page unreachable for non admins

--- FromLocalsToLocals/Controllers/AdminController.cs
+++ FromLocalsToLocals/Controllers/AdminController.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FromLocalsToLocals.Utilities.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace FromLocalsToLocals.Web.Controllers
 {
     public class AdminController : Controller
     {
+        [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
+        [Authorize(Roles = "Admin")]
         public IActionResult ReportPage()
         {
             return View();
