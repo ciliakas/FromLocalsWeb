945e59be18fee9a4fb2c9b72c1b95a223778702b
unknown <gytis.becalis@mif.stud.vu.lt>
2021-04-02 21:48:21 +0300

    basic design of admin page

---
+++ FromLocalsToLocals/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace FromLocalsToLocals.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ReportPage()
+        {
+            return View();
+        }
+    }
+}
