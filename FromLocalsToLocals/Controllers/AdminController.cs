8a29e30b806a25cd818daa535c6408764da4075f
Stokalas <gytis.becalis@gmail.com>
2021-04-03 21:26:12 +0300

    Some code for DB

--- FromLocalsToLocals/Controllers/AdminController.cs
+++ FromLocalsToLocals/Controllers/AdminController.cs
@@ -1,7 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FromLocalsToLocals.Contracts.Entities;
 using FromLocalsToLocals.Database;
+using FromLocalsToLocals.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace FromLocalsToLocals.Web.Controllers
@@ -9,6 +15,7 @@ namespace FromLocalsToLocals.Web.Controllers
     public class AdminController : Controller
     {
         private readonly AppDbContext _context;
+        public List<Report> Reports { get; set; }
 
         public AdminController(AppDbContext context)
         {
@@ -23,17 +30,31 @@ namespace FromLocalsToLocals.Web.Controllers
 
         [HttpGet]
         [Authorize(Roles = "Admin")]
-        public IActionResult ReportPage()
+        public async Task<IActionResult>  ReportPage()
         {
-            return View();
+            var reports = await _context.Reports.ToListAsync();
+            var model = new AdminViewModel
+            {
+                Reports = reports
+            };
+            return View(model);
         }
 
-        // [HttpPost]
-        // [Authorize(Roles = "Admin")]
-        // public async Task<IActionResult> CreateReport()
-        // {
-        //
-        // }
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateReport()
+        {
+            var report = new Report();
+            report.CreatedDate = DateTime.Now;
+            report.Category = 1;
+            report.Id = 4;
+            report.UserId = "useris";
+            report.Href = "google.com";
+            _context.Reports.Add(report);
+            Console.WriteLine("\n\n\n\nPridejau i db");
+
+            return View();
+        }
 
 
 
---
+++ FromLocalsToLocals/ViewModels/AdminViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FromLocalsToLocals.Contracts.Entities;
+
+namespace FromLocalsToLocals.Web.ViewModels
+{
+    public class AdminViewModel : Controller
+    {
+       public List<Report> Reports { get; set; }
+    }
+}
