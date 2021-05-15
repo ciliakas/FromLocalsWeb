ed96ef441f6d06adf642139dc63975d2a323ddf1
Stokalas <gytis.becalis@gmail.com>
2021-05-15 10:34:37 +0300

    card design

---
+++ FromLocalsToLocals/Controllers/SwipeCardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FromLocalsToLocals.Web.Controllers
+{
+    public class SwipeCardController : Controller
+    {
+        public IActionResult SwipeCard()
+        {
+            return View();
+        }
+    }
+}
