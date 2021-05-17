bb5697b042eb60cffe1fbd73430b10d15512111f
unknown <matas.lazdauskas@gmail.com>
2021-05-17 11:36:17 +0300

    Swipecard model

---
+++ FromLocalsToLocals.Contracts/Entities/SwipeCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using FromLocalsToLocals.Utilities.Enums;
+
+namespace FromLocalsToLocals.Contracts.Entities
+{
+    public class SwipeCard
+    {
+        public string Title { get; set; }
+        public byte[] Image { get; set; }
+        public VendorType VendorType { get; set; }
+        public string VendorName { get; set; }
+        public string Description { get; set; }
+    }
+}
--- FromLocalsToLocals/Controllers/SwipeCardController.cs
+++ FromLocalsToLocals/Controllers/SwipeCardController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Threading.Tasks;
+using FromLocalsToLocals.Contracts.Entities;
+using FromLocalsToLocals.Database;
+using FromLocalsToLocals.Services.EF;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FromLocalsToLocals.Web.Controllers
 {
     public class SwipeCardController : Controller
     {
-        public IActionResult SwipeCard()
+        private readonly IVendorService _vendorService;
+
+        public SwipeCardController(IVendorService vendorService)
         {
+            _vendorService = vendorService;
+        }
+
+        public async Task<List<SwipeCard>> GetSwipeCards(int Count)
+        {
+            var vendors = await _vendorService.GetNewVendorsAsync(Count);
+            var swipecards = vendors.Select(x => new SwipeCard { Id = x.ID ,Title = x.Title, Image = x.Image, VendorType = x.VendorType, VendorName = x.UserID, Description = x.About });
+            return swipecards.ToList();
+        }
+        public async Task<IActionResult> SwipeCard()
+        {
+            var swipecards = await GetSwipeCards(4);
             return View();
         }
     }
